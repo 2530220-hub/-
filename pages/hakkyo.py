55bc5a6044755b13b164d84ec469efe7668ee473
2530220-hub <2530220@sw.hs.kr>
2025-08-26 15:11:07 +0900

    Update hakkyo.py

--- pages/hakkyo.py
+++ pages/hakkyo.py
@@ -3,3 +3,5 @@ import streamlit as st
 
 st.title("포켓몬 타입 맞추기")
 st.image("https://upload.wikimedia.org/wikipedia/ko/thumb/e/eb/%ED%8F%AC%EC%BC%93%EB%AA%AC%EC%8A%A4%ED%84%B0_%EB%A0%88%EB%93%9C%C2%B7%EA%B7%B8%EB%A6%B0%EC%9D%98_%ED%99%8D%EB%B3%B4_%EC%9E%91%ED%92%88%EC%97%90_%EB%AC%98%EC%82%AC_%EB%90%9C_%ED%94%BC%EC%B9%B4%EC%B8%84.png/250px-%ED%8F%AC%EC%BC%93%EB%AA%AC%EC%8A%A4%ED%84%B0_%EB%A0%88%EB%93%9C%C2%B7%EA%B7%B8%EB%A6%B0%EC%9D%98_%ED%99%8D%EB%B3%B4_%EC%9E%91%ED%92%88%EC%97%90_%EB%AC%98%EC%82%AC_%EB%90%9C_%ED%94%BC%EC%B9%B4%EC%B8%84.png", caption="피카츄")
+if st.button("번개"):
+    st.write("옳다!")
